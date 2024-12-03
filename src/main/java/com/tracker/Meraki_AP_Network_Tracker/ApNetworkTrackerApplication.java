f94f06b38f66b9b2a75114d6e4207acaeb58d18e
jHallsG <janhalls202@gmail.com>
2024-12-03 11:49:09 +0800

    Initial commit. Start of project. Meraki Access Point Network Inventory and Deployment Tracker

---
+++ src/main/java/com/tracker/Meraki_AP_Network_Tracker/ApNetworkTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tracker.Meraki_AP_Network_Tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApNetworkTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApNetworkTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tracker/AP_Network_Tracker/ApNetworkTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracker.AP_Network_Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApNetworkTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
